9ee0fd06995257db50b646b5374a1264e0dc7a32
ZhangHang <zhanghang@meican.com>
2020-05-25 16:53:11 +0800

    wip

--- pkg/math/p.go
+++ pkg/math/p.go
@@ -1,7 +1,10 @@
 package math
 
-import m "github.com/zhanghang/gt/internal/pkg/math"
+// import m "github.com/zhanghang/gt/internal/pkg/math"
 
+// func Add(l, r int64) int64 {
+// 	return m.Add(l, r)
+// }
 func Add(l, r int64) int64 {
-	return m.Add(l, r)
+	return l + r
 }
